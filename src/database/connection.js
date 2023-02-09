33f637a2f3b0d81b78ecaf9ded2ee58156fb99f8
faruqismael <farusmith5@gmail.com>
2023-02-09 15:11:13 +0300

    hero overflow

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,16 +8,11 @@ module.exports = sequelize;
 global.sequelize = sequelize;
 */
 // server;
- const Sequelize = require("sequelize");
- const sequelize = new Sequelize(
-  'istyle',
-  'istyle_admin',
-  'Istyleaddis=1',
-   {
-     host: '127.0.0.1',
-     dialect: 'mysql',
-   }
- );
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("istyle", "istyle_admin", "Istyleaddis=1", {
+  host: "127.0.0.1",
+  dialect: "mysql",
+});
 
- module.exports = sequelize
- global.sequelize = sequelize
+module.exports = sequelize;
+global.sequelize = sequelize;
